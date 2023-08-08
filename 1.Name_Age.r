ebe55bea38c228ef415b77c01e0e0576f7c47bbc
KGnanasai <112232239+KGnanasai@users.noreply.github.com>
2023-08-08 09:20:47 +0530

    Delete 1.Name_Age.r

--- 1.Name_Age.r
+++
@@ -1,5 +0,0 @@
-name <- readline("Enter your name: ")
-age <- as.integer(readline("Enter your age: "))
-cat("Your name is:", name, "\n")
-cat("Your age is:", age, "\n")
-cat("R version:", R.version$version.string, "\n")
